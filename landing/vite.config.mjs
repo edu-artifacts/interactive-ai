9b5cb353588e53febcdd27608f4541607310ffda
Jonas <72263638+jonasneves@users.noreply.github.com>
2026-01-21 23:31:01 -0500

    Update vite.config.mjs

--- landing/vite.config.mjs
+++ landing/vite.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
     port: 3000,
     host: true,
     open: false,
+    sourcemapIgnoreList: (sourcePath) => sourcePath.includes('node_modules'),
   },
   esbuild: {
     target: 'es2022',
